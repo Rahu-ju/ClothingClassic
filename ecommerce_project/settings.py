72a0aa295fb97823f61cbc1af02cffb2a1fb9ce0
rahu-ju <rahu.exp@gmail.com>
2019-08-22 20:00:47 +0600

    Change allowed host.

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['clothingclassic.herokuapp.com', '127.0.0.1', 'localhost',]
 
 
 # Application definition
