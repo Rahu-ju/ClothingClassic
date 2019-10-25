05aa44b3e0ca0f98d89bded28812aeef89d629c2
rahu-ju <rahu.exp@gmail.com>
2019-10-26 03:31:29 +0600

    Add checkout work flow in template perspective.

--- directOrders/urls.py
+++ directOrders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import direct_checkout
+from .views import direct_checkout, order_confirm
 
 
 
 urlpatterns = [
     path('', direct_checkout, name='direct_order'),
+    path('order_confirm/', order_confirm, name='order_confirm'),
 ]
--- directOrders/views.py
+++ directOrders/views.py
@@ -5,3 +5,9 @@ def direct_checkout(request):
     template = 'checkout/direct_order_checkout.html'
     context = {}
     return render(request, template, context)
+
+
+def order_confirm(request):
+    template = 'checkout/order_confirm.html'
+    context = {}
+    return render(request, template, context)
