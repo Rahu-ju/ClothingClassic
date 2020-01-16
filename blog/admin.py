099929d08d9460e48dc4508759ac6e67996bf000
rahu-ju <rahu.exp@gmail.com>
2020-01-17 02:27:09 +0600

    Create feature post model, post model and comment.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import FeaturedPost, Post, Comment
+
 # Register your models here.
+admin.site.register(FeaturedPost)
+admin.site.register(Post)
+admin.site.register(Comment)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,54 @@
+from django.conf import settings 
 from django.db import models
 
 # Create your models here.
+def upload_blog_image(instance, filename):
+    return 'blog/images/%s/%s' % (instance.id, filename)
+
+
+class FeaturedPost(models.Model):
+    '''The post shows in the blog page as slider.'''
+    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    image = models.ImageField(upload_to=upload_blog_image)
+    date = models.DateField(auto_now=False, auto_now_add=True)
+    comment = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='comments', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Post(models.Model):
+
+    '''For ususal blog posts..'''
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=50)
+    description = models.TextField(max_length=200)
+    image = models.ImageField(upload_to=upload_blog_image)
+    date = models.DateField(auto_now=False, auto_now_add=True)
+    comment = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='post_comments', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Comment(models.Model):
+    '''To handle posts comment by annonymous or signed in user.'''
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(max_length=254, null=True, blank=True)
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+
+    
+
